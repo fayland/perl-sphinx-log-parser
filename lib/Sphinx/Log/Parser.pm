0f4d161f2a55d876ba7d031958e3eb15ab2033b3
fayland <fayland@gmail.com>
2009-10-20 15:56:02 +0800

    a working for 0.9.8.1

--- lib/Sphinx/Log/Parser.pm
+++ lib/Sphinx/Log/Parser.pm
@@ -31,10 +31,71 @@ sub new {
         }
     }
     else {
-        croak "argument must be either a file-name or an IO::Handle object.";
+        croak "argument must be either a file-name or an IO::Handle/File::Tail object.";
     }
 
     return bless \%data, $class;
+}
+
+sub _next_line {
+    my $self = shift;
+    my $f = $self->{file};
+    if(defined $self->{filetail}) {
+        return $f->read;
+    }
+    else {
+        return $f->getline;
+    }
+}
+
+sub next {
+    my ($self) = @_;
+    
+    while(defined (my $str = $self->_next_line)) {
+        # 0.9.8.1
+        # [Tue Oct 20 02:42:29.950 2009] 0.528 sec [ext/5/attr- 25863 (0,800)] [*] @sexual_preference Male -Female
+        # [query-date] query-time [match-mode/filters-count/sort-mode total-matches (offset,limit) @groupby-attr] [index-name] query
+
+        my @parts = split('\]', $str, 4);
+        # [
+        #  '[Tue Oct 20 02:42:29.979 2009',
+        #  ' 0.005 sec [ext/3/ext 163 (0,100)',
+        #  ' [*',
+        #  ' @city_id "3889"'
+        #];
+
+        #  '[Tue Oct 20 02:42:29.979 2009',
+        my $query_date = $parts[0]; $query_date =~ s/^\[//;
+        #  ' 0.005 sec [ext/3/ext 163 (0,100)',
+        $parts[1] =~ /^\s*([\d\.]+)\s+sec\s+\[(\w+)\/(\d+)\/([\w\-\+]+)\s(\d+)\s\((\d+)\,(\d+)\)\s*\@?(\S+)?$/;
+        my $query_time = $1;
+        my $match_mode = $2;
+        my $filter_count = $3;
+        my $sort_mode  = $4;
+        my $total_matches = $5;
+        my $offset = $6;
+        my $limit  = $7;
+        my $groupby_attr = $8;
+        #  ' [*',
+        my $index_name = $parts[2]; $index_name =~ s/^\s*\[//;
+        #  ' @city_id "3889"'
+        my $query = $parts[3]; $query =~ s/(^\s+|\s+$)//g;
+
+        return {
+            query_date => $query_date,
+            query_time => $query_time,
+            match_mode => $match_mode,
+            filter_count => $filter_count,
+            sort_mode  => $sort_mode,
+            total_matches => $total_matches,
+            offset => $offset,
+            limit  => $limit,
+            groupby_attr => $groupby_attr,
+            index_name => $index_name,
+            query  => $query
+        };
+    }
+    return;
 }
 
 1;
@@ -42,6 +103,11 @@ sub new {
 =head1 SYNOPSIS
 
     use Sphinx::Log::Parser;
+    
+    my $parser = Sphinx::Log::Parser->new( '/var/log/searchd/query.log' );
+    while (my $sl = $parser->next) {
+        print $sl->{total_matches}, $sl->{query_date}, "\n"; # more
+    }
 
 =head1 DESCRIPTION
 
--- t/basic.t
+++ t/basic.t
@@ -2,19 +2,72 @@
 
 use strict;
 use warnings;
-use Test::More tests => 1;
+use Test::More tests => 5;
 use Sphinx::Log::Parser;
+use FindBin qw/$Bin/;
 
-ok(1);
+my $parser = Sphinx::Log::Parser->new( "$Bin/0.9.8.log" );
+my @sls;
+while (my $sl = $parser->next) {
+    push @sls, $sl;
+}
 
-# 0.9.8.1
-my $log = <<LOG;
-[Tue Oct 20 02:42:29.950 2009] 0.528 sec [ext/5/attr- 25863 (0,800)] [*] @sexual_preference Male -Female
-[Tue Oct 20 02:42:29.979 2009] 0.005 sec [ext/3/ext 163 (0,100)] [*] @city_id "3889"
-[Tue Oct 20 02:42:30.052 2009] 0.048 sec [scan/4/attr- 106 (64,16)] [*]
-[Tue Oct 20 02:42:30.086 2009] 0.001 sec [ext/0/attr- 0 (0,700)] [*] @username ("francine | cassiday")
-[Tue Oct 20 02:42:30.090 2009] 0.001 sec [ext/0/attr- 0 (0,700)] [*] @first_name ("francine | cassiday")
-LOG
-
+is scalar @sls, 4;
+# [Fri Jun 29 21:17:58 2007] 0.004 sec [all/0/rel 35254 (0,20)] [lj] test
+is_deeply $sls[0], {
+  'total_matches' => '35254',
+  'match_mode' => 'all',
+  'query' => 'test',
+  'query_date' => 'Fri Jun 29 21:17:58 2007',
+  'filter_count' => '0',
+  'index_name' => 'lj',
+  'limit' => '20',
+  'query_time' => '0.004',
+  'sort_mode' => 'rel',
+  'groupby_attr' => undef,
+  'offset' => '0'
+};
+# [Fri Jun 29 21:20:34 2007] 0.024 sec [all/0/rel 19886 (0,20) @channel_id] [lj] test
+is_deeply $sls[1], {
+  'total_matches' => '19886',
+  'match_mode' => 'all',
+  'query' => 'test',
+  'query_date' => 'Fri Jun 29 21:20:34 2007',
+  'filter_count' => '0',
+  'index_name' => 'lj',
+  'limit' => '20',
+  'query_time' => '0.024',
+  'sort_mode' => 'rel',
+  'groupby_attr' => 'channel_id',
+  'offset' => '0'
+};
+# [Tue Oct 20 02:42:29.979 2009] 0.005 sec [ext/3/ext 163 (0,100)] [*] @city_id "3889"
+is_deeply $sls[2], {
+  'total_matches' => '163',
+  'match_mode' => 'ext',
+  'query' => '@city_id "3889"',
+  'query_date' => 'Tue Oct 20 02:42:29.979 2009',
+  'filter_count' => '3',
+  'index_name' => '*',
+  'limit' => '100',
+  'query_time' => '0.005',
+  'sort_mode' => 'ext',
+  'groupby_attr' => undef,
+  'offset' => '0'
+};
+# [Tue Oct 20 02:42:30.052 2009] 0.048 sec [scan/4/attr- 106 (64,16)] [*]
+is_deeply $sls[3], {
+  'total_matches' => '106',
+  'match_mode' => 'scan',
+  'query' => '',
+  'query_date' => 'Tue Oct 20 02:42:30.052 2009',
+  'filter_count' => '4',
+  'index_name' => '*',
+  'limit' => '16',
+  'query_time' => '0.048',
+  'sort_mode' => 'attr-',
+  'groupby_attr' => undef,
+  'offset' => '64'
+};
 
 1;
