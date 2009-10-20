6f04d851de163c8e89903f065d64c6811b14b179
fayland <fayland@gmail.com>
2009-10-20 14:46:23 +0800

    blabla

--- lib/Sphinx/Log/Parser.pm
+++ lib/Sphinx/Log/Parser.pm
@@ -5,6 +5,37 @@ use warnings;
 
 # ABSTRACT: parse Sphinx searchd log
 
+use Carp;
+use IO::File;
+use IO::Handle;
+
+sub new {
+    my ($class, $file) = @_;
+
+    my %data;
+    if(UNIVERSAL::isa($file, 'IO::Handle')) {
+        $data{file} = $file;
+    }
+    elsif(UNIVERSAL::isa($file, 'File::Tail')) {
+        $data{file} = $file;
+        $data{filetail}=1;
+    }
+    elsif(! ref $file) {
+        if($file eq '-') {
+            my $io = new IO::Handle;
+            $data{file} = $io->fdopen(fileno(STDIN),"r");
+        }
+        else {
+            $data{file} = new IO::File($file, "<");
+            defined $data{file} or croak "can't open $file: $!";
+        }
+    }
+    else {
+        croak "argument must be either a file-name or an IO::Handle object.";
+    }
+
+    return bless \%data, $class;
+}
 
 1;
 
--- t/basic.t
+++ t/basic.t
@@ -1,9 +1,20 @@
 #!/usr/bin/perl
 
+use strict;
+use warnings;
 use Test::More tests => 1;
-
+use Sphinx::Log::Parser;
 
 ok(1);
+
+# 0.9.8.1
+my $log = <<LOG;
+[Tue Oct 20 02:42:29.950 2009] 0.528 sec [ext/5/attr- 25863 (0,800)] [*] @sexual_preference Male -Female
+[Tue Oct 20 02:42:29.979 2009] 0.005 sec [ext/3/ext 163 (0,100)] [*] @city_id "3889"
+[Tue Oct 20 02:42:30.052 2009] 0.048 sec [scan/4/attr- 106 (64,16)] [*]
+[Tue Oct 20 02:42:30.086 2009] 0.001 sec [ext/0/attr- 0 (0,700)] [*] @username ("francine | cassiday")
+[Tue Oct 20 02:42:30.090 2009] 0.001 sec [ext/0/attr- 0 (0,700)] [*] @first_name ("francine | cassiday")
+LOG
 
 
 1;
